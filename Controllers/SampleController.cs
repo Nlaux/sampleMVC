3ee40b319cc44f53c391496afa0b5ac6a78bb45b
Nicholas Laux <Laux-N@iehp.org>
2018-03-21 14:09:45 -0700

    first push

---
+++ Controllers/SampleController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using SampleMvc.Models;
+
+namespace SampleMvc.Controllers
+{
+    public class SampleController : Controller
+    {
+        public ActionResult SampleMvcInnerSublayout()
+        {
+            var model = new SampleMvcInnerSublayoutViewModel
+            {
+                CurrentYear = Sitecore.DateUtil.ToServerTime(DateTime.UtcNow).Year
+            };
+
+            var guidValue = Sitecore.Configuration.Settings.GetSetting("parentGuid");
+
+            ViewBag.Message = guidValue;
+
+            return View(model);
+        }
+    }
+}
