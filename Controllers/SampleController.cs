f21258aeacd7a96999b5dd73a8c50ffa217032b9
Nicholas Laux <Laux-N@iehp.org>
2018-03-27 08:15:53 -0700

    working version.

--- Controllers/SampleController.cs
+++ Controllers/SampleController.cs
@@ -1,22 +1,47 @@
-﻿using System;
+﻿using Sitecore;
+using Sitecore.Data;
+using Sitecore.Data.Items;
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using Sitecore.Mvc.Controllers;
 using System.Web.Mvc;
 using SampleMvc.Models;
 
 namespace SampleMvc.Controllers
 {
-    public class SampleController : Controller
+    public class SampleController : SitecoreController
     {
         public ActionResult SampleMvcInnerSublayout()
         {
+            //ignore this block, not sure how to delete w/o breaking the view
             var model = new SampleMvcInnerSublayoutViewModel
             {
                 CurrentYear = Sitecore.DateUtil.ToServerTime(DateTime.UtcNow).Year
             };
 
-            var guidValue = Sitecore.Configuration.Settings.GetSetting("parentGuid");
 
-            ViewBag.Message = guidValue;
+            //read in config file & get parentGuid value
+            var parentGuidValue = Sitecore.Configuration.Settings.GetSetting("parentGuid");
+
+            //init Sitecore db 
+            Database database = Context.Database;
+            Item dataHolder = database.GetItem(parentGuidValue);
+
+            //build list of children Guids from parentGuid - Not sure if it's working or not.
+            //List<ID> templateList = dataHolder.Children.Where(x => x.TemplateID.ToString() == parentGuidValue).Select(i => i.ID).ToList();
+
+            //pass parent guid from config file to view
+            ViewBag.parentGuid = parentGuidValue;
+
+            //pass templateID from parent Guid to view
+            ViewBag.templateID = dataHolder.TemplateID;
+
+            //pass children list from parent Guid to view
+            ViewBag.tempList = dataHolder.Children;
+
+            //pass item headline from current itemID to view
+            //ViewBag.itemHeadline = dataHolder.Fields["Headline"];
 
             return View(model);
         }
